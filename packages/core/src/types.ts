8eff17deceb44913c9ac223ad03798b595a875c0
koteelok <koteeldev@gmail.com>
2026-01-03 12:52:32 +0300

    Add metadata to File

--- packages/core/src/types.ts
+++ packages/core/src/types.ts
@@ -2,6 +2,7 @@ export type ArrayOr<T> = T | T[];
 export type MaybePromise<T> = T | Promise<T>;
 export type QueryLike = string | string[];
 export interface File {
+  metadata?: any;
   dirname: string;
   basename: string;
   content: string;
--- packages/core/src/utils/clone.ts
+++ packages/core/src/utils/clone.ts
@@ -37,6 +37,7 @@ export function cloneFiles(files: File[]): File[] {
   const clone: File[] = [];
   for (const file of files) {
     clone.push({
+      metadata: file.metadata,
       basename: file.basename,
       dirname: file.dirname,
       content: file.content,
