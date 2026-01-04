dd127a61ba1a6730d693afd5916973c342efd404
koteelok <koteeldev@gmail.com>
2026-01-04 12:32:24 +0300

    Add some fake classes to make types more pleasable to read

--- packages/config/src/api.ts
+++ packages/config/src/api.ts
@@ -1,11 +1,4 @@
-import {
-  ContextPipeline,
-  FilesPipeline,
-  GroupPipeline,
-  IgnorePipeline,
-  InteractivePipeline as _InteractivePipeline,
-  QueryPipeline,
-} from "@assetpipe/core/pipelines";
+import * as mixins from "@assetpipe/core/pipelines";
 import { ArrayOr, File, QueryLike, Transformer } from "@assetpipe/core/types";
 
 // Fake API-like classes to make user comfortable lol
@@ -14,7 +7,7 @@ class Pipeline {}
 
 class InteractivePipeline {
   pipe(transformer: Transformer) {
-    const self = this as unknown as _InteractivePipeline;
+    const self = this as unknown as mixins.InteractivePipeline;
     self.commands.push({
       type: "pipe",
       transformer,
@@ -24,7 +17,7 @@ class InteractivePipeline {
   }
 
   branch(...transformers: ArrayOr<Transformer>[]) {
-    const self = this as unknown as _InteractivePipeline;
+    const self = this as unknown as mixins.InteractivePipeline;
     self.commands.push({
       type: "branch",
       transformers: transformers.flat(),
@@ -34,9 +27,9 @@ class InteractivePipeline {
   }
 
   pull(...pipelines: InteractivePipeline[]) {
-    const self = this as unknown as _InteractivePipeline;
+    const self = this as unknown as mixins.InteractivePipeline;
     for (const pipeline of pipelines) {
-      if (!_InteractivePipeline.is(pipeline)) {
+      if (!mixins.InteractivePipeline.is(pipeline)) {
         throw new Error(
           `Passed argument ${pipeline} is not actually a pipeline!`
         );
@@ -44,7 +37,7 @@ class InteractivePipeline {
 
       self.commands.push({
         type: "pull",
-        pipeline: pipeline as unknown as _InteractivePipeline,
+        pipeline: pipeline as unknown as mixins.InteractivePipeline,
       });
     }
 
@@ -52,42 +45,44 @@ class InteractivePipeline {
   }
 }
 
+class QueryPipeline extends InteractivePipeline {}
+
 export const select = {
   parallel(query: QueryLike) {
-    const pipeline = new InteractivePipeline();
-    QueryPipeline.mixin(pipeline, { query });
+    const pipeline = new QueryPipeline();
+    mixins.QueryPipeline.mixin(pipeline, { query });
     return pipeline;
   },
 
   bulk(query: QueryLike) {
-    const pipeline = new InteractivePipeline();
-    QueryPipeline.mixin(pipeline, { query, bulk: true });
+    const pipeline = new QueryPipeline();
+    mixins.QueryPipeline.mixin(pipeline, { query, bulk: true });
     return pipeline;
   },
 
   groupBy(query: QueryLike, callback: (file: File) => string) {
-    const pipeline = new InteractivePipeline();
-    QueryPipeline.mixin(pipeline, { query, groupBy: callback });
+    const pipeline = new QueryPipeline();
+    mixins.QueryPipeline.mixin(pipeline, { query, groupBy: callback });
     return pipeline;
   },
 };
 
 export const claim = {
   parallel(query: QueryLike) {
-    const pipeline = new InteractivePipeline();
-    QueryPipeline.mixin(pipeline, { query, claim: true });
+    const pipeline = new QueryPipeline();
+    mixins.QueryPipeline.mixin(pipeline, { query, claim: true });
     return pipeline;
   },
 
   bulk(query: QueryLike) {
-    const pipeline = new InteractivePipeline();
-    QueryPipeline.mixin(pipeline, { query, bulk: true, claim: true });
+    const pipeline = new QueryPipeline();
+    mixins.QueryPipeline.mixin(pipeline, { query, bulk: true, claim: true });
     return pipeline;
   },
 
   groupBy(query: QueryLike, callback: (file: File) => string) {
-    const pipeline = new InteractivePipeline();
-    QueryPipeline.mixin(pipeline, {
+    const pipeline = new QueryPipeline();
+    mixins.QueryPipeline.mixin(pipeline, {
       query,
       groupBy: callback,
       claim: true,
@@ -96,32 +91,37 @@ export const claim = {
   },
 };
 
-export function ignore(query: QueryLike): Pipeline {
-  const pipeline = new Pipeline();
-  IgnorePipeline.mixin(pipeline, { query });
+class IgnorePipeline extends Pipeline {}
+
+export function ignore(query: QueryLike) {
+  const pipeline = new IgnorePipeline();
+  mixins.IgnorePipeline.mixin(pipeline, { query });
   return pipeline;
 }
 
-export function context(
-  root: string,
-  ...pipelines: ArrayOr<Pipeline>[]
-): InteractivePipeline {
-  const pipeline = new InteractivePipeline();
-  ContextPipeline.mixin(pipeline, {
+class FilesPipeline extends InteractivePipeline {}
+
+export function files(files: File[]) {
+  const pipeline = new FilesPipeline();
+  mixins.FilesPipeline.mixin(pipeline, { files });
+  return pipeline;
+}
+
+class GroupPipeline extends InteractivePipeline {}
+
+export function group(...pipelines: ArrayOr<Pipeline>[]) {
+  const pipeline = new GroupPipeline();
+  mixins.GroupPipeline.mixin(pipeline, { children: pipelines.flat() as any[] });
+  return pipeline;
+}
+
+class ContextPipeline extends GroupPipeline {}
+
+export function context(root: string, ...pipelines: ArrayOr<Pipeline>[]) {
+  const pipeline = new ContextPipeline();
+  mixins.ContextPipeline.mixin(pipeline, {
     context: root,
     children: pipelines.flat() as any[],
   });
-  return pipeline;
-}
-
-export function files(files: File[]): InteractivePipeline {
-  const pipeline = new InteractivePipeline();
-  FilesPipeline.mixin(pipeline, { files });
-  return pipeline;
-}
-
-export function group(...pipelines: ArrayOr<Pipeline>[]): InteractivePipeline {
-  const pipeline = new InteractivePipeline();
-  GroupPipeline.mixin(pipeline, { children: pipelines.flat() as any[] });
   return pipeline;
 }
