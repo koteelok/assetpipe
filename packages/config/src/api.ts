db24a1ce8ed2d0012d610f7ae5fe60dc0a458ca4
koteelok <grellkasweet@gmail.com>
2026-04-02 04:37:46 +0300

    Add jsdocs to apis

--- packages/config/src/api.ts
+++ packages/config/src/api.ts
@@ -6,6 +6,7 @@ import type { ArrayOr, File, Transformer } from "@assetpipe/core/types";
 class Pipeline {}
 
 class InteractivePipeline {
+  /** Apply a transformer to the pipeline's files. */
   pipe(transformer: Transformer) {
     const self = this as unknown as mixins.InteractivePipeline;
     self.commands.push({
@@ -16,6 +17,7 @@ class InteractivePipeline {
     return this;
   }
 
+  /** Fork the pipeline into multiple transformers that each receive the same input. */
   branch(...transformers: ArrayOr<Transformer>[]) {
     const self = this as unknown as mixins.InteractivePipeline;
     self.commands.push({
@@ -26,6 +28,7 @@ class InteractivePipeline {
     return this;
   }
 
+  /** Pull results from other pipelines into this one. */
   pull(...pipelines: InteractivePipeline[]) {
     const self = this as unknown as mixins.InteractivePipeline;
     for (const pipeline of pipelines) {
@@ -55,11 +58,15 @@ declare global {
 }
 
 interface QueryOptions extends AssetpipeMixins.QueryOptions {
+  /** Take ownership of matched files so later queries on the same glob see nothing. */
   claim?: boolean;
+  /** Pass all matched files to the transformer at once instead of one at a time. */
   bulk?: boolean;
+  /** Group matched files by key and run the transformer once per group. */
   groupBy?: (file: File) => string;
 }
 
+/** Select files matching a glob pattern and create a pipeline to process them. */
 export function query(query: QueryLike, options: QueryOptions) {
   const pipeline = new QueryPipeline();
   mixins.QueryPipeline.mix(pipeline, {
@@ -71,6 +78,7 @@ export function query(query: QueryLike, options: QueryOptions) {
 
 class IgnorePipeline extends Pipeline {}
 
+/** Exclude files matching a glob pattern from all other pipelines. */
 export function ignore(query: QueryLike) {
   const pipeline = new IgnorePipeline();
   mixins.IgnorePipeline.mix(pipeline, {
@@ -81,6 +89,7 @@ export function ignore(query: QueryLike) {
 
 class GroupPipeline extends InteractivePipeline {}
 
+/** Combine multiple pipelines so they run together and their outputs are merged. */
 export function group(...pipelines: ArrayOr<Pipeline>[]) {
   const pipeline = new GroupPipeline();
   mixins.GroupPipeline.mix(pipeline, { children: pipelines.flat() as any[] });
@@ -89,6 +98,7 @@ export function group(...pipelines: ArrayOr<Pipeline>[]) {
 
 class ContextPipeline extends GroupPipeline {}
 
+/** Set a root directory so inner pipeline queries resolve relative to it. */
 export function context(root: string, ...pipelines: ArrayOr<Pipeline>[]) {
   const pipeline = new ContextPipeline();
   mixins.ContextPipeline.mix(pipeline, {
